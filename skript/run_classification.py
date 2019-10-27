d47928ecb2fcf37a3112af6a3f2c9579feb53a04
romeolandry <kamgoche@yahoo.com>
2019-10-28 00:49:08 +0100

    Update run_classification.py

--- skript/run_classification.py
+++ skript/run_classification.py
@@ -26,6 +26,11 @@ parser.add_option("-s", "--save",
                   help="pfad zur Speicherung des Models",
                   default=cfg.path_to_model)
 
+parser.add_option("-t", "--gen",
+                  dest="data_gen",
+                  help="train Model with data generator",
+                  default=False)
+
 (options, args) = parser.parse_args()
 
 
@@ -49,11 +54,6 @@ print("#####################################")
 print("###### Trainingsphase #############")
 print("##################################")
 
-losses = ['categorical_crossentropy',
-          'kullback_leibler_divergence']
-lernrates = [1e-3, 1e-4, 1e-5, 1e-6]
-count = 0
-
 # image und Labels zum Trainen werden gelesen
 print("Daten werden eingelesen.....")
 (train_images,
@@ -61,49 +61,50 @@ print("Daten werden eingelesen.....")
     classes) = data_vorbereitung.load_roadsigns_data(1)
 print("Daten eingelesen!")
 # data_vorbereitung.display_roadsign_classes(train_images, 43)
-for loss in losses:
-    for lernrate in lernrates:
-        # Vorbereitung des Models
-        print("Aufbau des Models!")
-        cfg.keras_model_name = "keras_model_{}".format(count)
-        save_model_to = os.path.join(
-            pfad_des_model,
-            cfg.keras_model_name + ".h5")
 
-        optimizer = optimizers.Adamax(cfg.lernrate)
-        train_model = Classification(save_model_to,
-                                     cfg.IMG_SIZE,
-                                     classes,
-                                     loss,
-                                     optimizer,
-                                     cfg.metrics,
-                                     cfg.verborse,
-                                     cfg.validation_split,
-                                     cfg.NUM_BATCH,
-                                     cfg.NUM_EPOCHS)
+print("Aufbau des Models!")
+cfg.keras_model_name = "keras_model_sign"
+save_model_to = os.path.join(
+    pfad_des_model,
+    cfg.keras_model_name + ".h5")
 
-        model = train_model.build_model()
-        print("train model :", cfg.keras_model_name)
+optimizer = optimizers.Adamax(cfg.lernrate)
+train_model = Classification(save_model_to,
+                             cfg.IMG_SIZE,
+                             classes,
+                             loss,
+                             optimizer,
+                             cfg.metrics,
+                             cfg.verborse,
+                             cfg.validation_split,
+                             cfg.NUM_BATCH,
+                             cfg.NUM_EPOCHS)
 
-        """ history = train_model.train_model(model,
-                                                train_images,
-                                                train_labels) """
+model = train_model.build_model()
+print("train model :", cfg.keras_model_name)
+if options.data_gen:
+    print(options.data_gen)
+    print("train wih generator")
+    exit()
+    history = train_model.train_model_with_data_generator(
+            model,
+            train_images,
+            train_labels)
+else:
+    print("simple training!")
+    exit()
+    history = train_model.train_model(model,
+                                      train_images,
+                                      train_labels)
 
-        history = train_model.train_model_with_data_generator(
-                    model,
-                    train_images,
-                    train_labels)
+print("##### max validation acc :", min(history.history['val_acc']))
+print("##### min val_loss :", max(history.history['val_loss']))
 
-        print("##### max validation acc :", min(history.history['val_acc']))
-        print("##### min val_loss :", max(history.history['val_loss']))
-
-        save_performance_model(cfg.keras_model_name + str(count),
-                               loss,
-                               lernrate,
-                               max(history.history['val_acc']),
-                               min(history.history['val_loss']))
-        count += 1
-
+save_performance_model(cfg.keras_model_name + str(count),
+                       loss,
+                       lernrate,
+                       max(history.history['val_acc']),
+                       min(history.history['val_loss']))
 
 print("save resume of validation and losses")
 plot_performance_models(cfg.pfad_zu_performance_model)
